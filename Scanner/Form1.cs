3d80fe1b79870b20011915d3562288e64e57e398
student <student@PC-218>
2017-05-25 16:21:00 +0200

    Basic scanner is complete

--- Scanner/Form1.cs
+++ Scanner/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Scanner
 {
@@ -15,6 +17,28 @@ namespace Scanner
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Scan_Click(object sender, EventArgs e)
+        {
+            short[] ListaPortow = { 20, 21, 22, 55, 66, 67, 68, 89, 99 };
+            string host = textBox1.Text;
+            listBox1.Items.Add("Scanning for" + host);
+            listBox1.Items.Add("This might take a while!");
+            foreach(short port in ListaPortow)
+            {
+                this.Refresh();
+                try
+                {
+                    TcpClient klient = new TcpClient(host, port);
+                    listBox1.Items.Add("Port: " + port.ToString() + " is open");
+                }
+                catch
+                {
+                    listBox1.Items.Add("Port: " + port.ToString() + " is closed.");
+                }
+            }
+
         }
     }
 }
